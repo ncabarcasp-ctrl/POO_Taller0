9cae89e6b01817c12b4558d350e99cbfed58eb9a
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 17:30:19 -0500

    feat(bloqueB-ej8): crear clase producto

---
+++ Ejercicio8.java
@@ -0,0 +1,15 @@
+class Producto {
+    String nombre;
+    double precio;
+    int stock;
+
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + stock);
+    }
+}
+
+public class Ejercicio8 { 
+    
+}
