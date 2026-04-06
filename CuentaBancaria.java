e508c88618046af3c6e772cad4e4e3d96f1e0d69
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 15:51:10 -0500

    feat(bloqueA-ej4): agregar método mostrarCuenta

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,4 +2,11 @@ public class CuentaBancaria {
     String titular;
     String numeroCuenta;
     double saldo;
+
+    void mostrarCuenta() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Número de Cuenta: " + numeroCuenta);
+        System.out.println("Saldo: $" + saldo);
+    }
+
 }
