a379ea273fffbd55e3a0be716a97439c9738720f
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 17:09:32 -0500

    feat(bloqueB-ej6): asignar valores y mostrar información

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -3,7 +3,7 @@ class Estudiante {
     int codigo;
     String semestre;
 
-    void mostrarInformacion() {
+    void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Código: " + codigo);
         System.out.println("Semestre: " + semestre);
@@ -14,6 +14,10 @@ public class Ejercicio6 {
     public static void main(String[] args)
     {
         Estudiante estudiante1 = new Estudiante();
+        estudiante1.nombre = "Natalia";
+        estudiante1.codigo = 12345;
+        estudiante1.semestre = "Segundo";
+        estudiante1.mostrarInfo();
     }
     
 }
