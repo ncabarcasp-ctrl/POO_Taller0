6467d22a04b33ad54304e27dc1abd46a69882d8f
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 14:15:28 -0500

    feat(bloqueA-ej2): agregar método mostrarEstado

--- Vehiculo.java
+++ Vehiculo.java
@@ -2,4 +2,10 @@ public class Vehiculo {
     String marca;
     String modelo;
     int velocidadActual;
+
+    void mostrarEstado() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+    }
 }
