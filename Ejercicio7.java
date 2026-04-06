55d067235208934d5a273334f049a6181222b8db
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 17:16:26 -0500

    feat(bloqueB-ej7): crear clase base vehiculo

---
+++ Ejercicio7.java
@@ -0,0 +1,16 @@
+class Vehiculo {
+    String marca;
+    String modelo;
+    int velocidadActual;
+
+    void mostrarEstado() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+    }
+}
+
+public class Ejercicio7 {
+
+}
+    
