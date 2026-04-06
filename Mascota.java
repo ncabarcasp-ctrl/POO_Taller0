2cf949d7628d3944ba3fc0bfc980b2a005baa3af
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 16:10:01 -0500

    feat(bloqueA-ej5): agregar método mostrarMascota

--- Mascota.java
+++ Mascota.java
@@ -2,4 +2,10 @@ public class Mascota {
     String nombre;
     String especie;
     int edad;
+
+    void mostrarMascota() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+    }
 }
