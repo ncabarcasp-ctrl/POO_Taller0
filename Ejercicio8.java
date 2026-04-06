8d52c9cca6e853e1ccfefcf5b2c4eb09ac703252
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 17:38:14 -0500

    feat(bloqueB-ej8): asignar estados distintos a tres productos

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -15,5 +15,21 @@ public class Ejercicio8 {
         Producto producto1 = new Producto();
         Producto producto2 = new Producto();
         Producto producto3 = new Producto();
+
+        producto1.nombre = "Laptop";
+        producto1.precio = 999.99;
+        producto1.stock = 10;
+
+        producto2.nombre = "Smartphone";
+        producto2.precio = 499.99;
+        producto2.stock = 20;
+
+        producto3.nombre = "Tablet";
+        producto3.precio = 299.99;
+        producto3.stock = 15;
+
+        producto1.mostrarInformacion();
+        producto2.mostrarInformacion();
+        producto3.mostrarInformacion();
     } 
 }
