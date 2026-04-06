eebd332f82cc39d158f6ce5d6d5c0144233a8eea
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 18:29:36 -0500

    feat(bloqueB-ej10): crear dos objetos cuenta bancaria

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -6,8 +6,12 @@ class CuentaBancaria {
     void mostrarCuenta() {
         System.out.println("Número de cuenta: " + numero);
         System.out.println("Titular: " + titular);
-        System.out.println("Saldo: " + saldo);
+        System.out.println("Saldo: $" + saldo);
     }
 }
  public class Ejercicio10 {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria();
+        CuentaBancaria cuenta2 = new CuentaBancaria();
     }
+}
