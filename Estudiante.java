47d6e6a70451a876e06acb4a92f2906118c85da2
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 13:32:19 -0500

    feat(bloqueA-ej1): agregar método mostrarInfo

--- Estudiante.java
+++ Estudiante.java
@@ -3,4 +3,10 @@ public class Estudiante {
     int codigo;
     String semestre;
 
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Código: " + codigo);
+        System.out.println("Semestre: " + semestre);
+    }
+
 }
