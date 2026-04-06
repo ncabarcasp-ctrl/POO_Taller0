e480d36e184d9bb24ac2dc6489eabf7c51708322
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 16:56:56 -0500

    feat(bloqueB-ej6): crear clase base estudiante

---
+++ Ejercicio6.java
@@ -0,0 +1,16 @@
+class Estudiante {
+    String nombre;
+    int codigo;
+    String semestre;
+
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Código: " + codigo);
+        System.out.println("Semestre: " + semestre);
+    }
+}
+
+public class Ejercicio6 {
+    
+    
+}
