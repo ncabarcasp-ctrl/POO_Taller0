34949556671e7ba5370847a0be8ac8863e6b3ca7
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 18:26:28 -0500

    feat(bloqueB-ej10): crear clase base cuenta bancaria

---
+++ Ejercicio10.java
@@ -0,0 +1,13 @@
+class CuentaBancaria {
+    String numero;
+    String titular;
+    double saldo;
+
+    void mostrarCuenta() {
+        System.out.println("Número de cuenta: " + numero);
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: " + saldo);
+    }
+}
+ public class Ejercicio10 {
+    }
