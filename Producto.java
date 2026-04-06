6f15b16ab218f99e73116fff74b1bd219e275258
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 14:56:32 -0500

    feat(bloqueA-ej3): agregar método mostrarProducto

--- Producto.java
+++ Producto.java
@@ -2,4 +2,10 @@ public class Producto {
     String nombre;
     double precio;
     int stock;
+
+    void mostrarProducto() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + stock);
+    }
 }
