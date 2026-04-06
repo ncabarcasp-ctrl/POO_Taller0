2fd6b0f6b54427b7e5ccd75051ca897424c19b88
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 18:18:22 -0500

    feat(bloqueB-ej9): cambiar dos veces y mostrar mascota

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -3,7 +3,7 @@ class Mascota {
     String especie;
     int edad;
 
-    void mostrarInformacion() {
+    void mostrarMascota() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Especie: " + especie);
         System.out.println("Edad: " + edad + " años");
@@ -14,5 +14,15 @@ public class Ejercicio9 {
     public static void main(String[] args)
     {
         Mascota mascota = new Mascota(); 
+        mascota.nombre = "Firulais";
+        mascota.especie = "Perro";
+        mascota.edad = 3;
+        mascota.mostrarMascota();
+
+        mascota.edad = 5;
+        mascota.mostrarMascota();
+
+        mascota.edad = 2;
+        mascota.mostrarMascota();
     }
 }
