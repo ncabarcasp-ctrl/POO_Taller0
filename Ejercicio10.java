7934f5223ba0be80d32b3f54d62b02f06b8483df
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 18:33:27 -0500

    feat(bloqueB-ej10): asignar valores independientes a dos cuentas

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -13,5 +13,16 @@ class CuentaBancaria {
     public static void main(String[] args) {
         CuentaBancaria cuenta1 = new CuentaBancaria();
         CuentaBancaria cuenta2 = new CuentaBancaria();
+
+        cuenta1.numero = "123456789";
+        cuenta1.titular = "Natalia Cabarcas";
+        cuenta1.saldo = 1500.75;
+
+        cuenta2.numero = "987654321";
+        cuenta2.titular = "Karol G";
+        cuenta2.saldo = 2500.00;
+
+        cuenta1.mostrarCuenta();
+        cuenta2.mostrarCuenta();
     }
 }
