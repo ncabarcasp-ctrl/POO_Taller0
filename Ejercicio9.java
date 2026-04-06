c231d4210ef06fbff418495ea61f907bad6e8df1
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 18:03:13 -0500

    feat(bloqueB-ej9): crear clase base mascota

---
+++ Ejercicio9.java
@@ -0,0 +1,14 @@
+class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+    }
+}
+
+public class Ejercicio9 {
+}
