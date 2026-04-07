853ccdf25e833cbf42b272c87a116dbced807f67
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-07 16:10:10 -0500

    feat(bloqueC-ej18): probar metodo cumplirArnios incrementando la edad de la mascota

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -15,5 +15,19 @@ class Mascota {
 }
 
 public class Ejercicio18 {
-    
+    public static void main(String[] args) {
+        Mascota miMascota = new Mascota();
+        miMascota.nombre = "Firulais";
+        miMascota.especie = "Perro";
+        miMascota.edad = 3; // Edad inicial
+
+        System.out.println("EDAD INICIAL:");
+        miMascota.mostrarMascota();
+
+        // Llega el cumpleaños de la mascota
+        System.out.println("\n¡Es el cumpleaños de " + miMascota.nombre + "!");
+        miMascota.cumplirAnios(); // Incrementa la edad y muestra el mensaje de cumpleaños
+        System.out.println("EDAD DESPUÉS DEL CUMPLEAÑOS:");
+        miMascota.mostrarMascota(); // Muestra la información actualizada de la mascota después del cumpleaños
+    }
 }
