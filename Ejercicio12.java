71bcc46d55e36d80e63a73ea8f00c1a226fddd97
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 22:15:02 -0500

    feat(bloqueC-ej12): probar método frenar con dos autos para validar independencia y límite minimo

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -20,4 +20,27 @@ class Vehiculo {
 }
 
 public class Ejercicio12 {
+    public static void main(String[] args) {
+        Vehiculo carro1 = new Vehiculo();
+        Vehiculo carro2 = new Vehiculo();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.velocidadActual = 5;
+
+        carro2.marca = "Honda";
+        carro2.modelo = "Civic";
+        carro2.velocidadActual = 80;
+
+        System.out.println("--- ANTES DE FRENAR ---");
+        carro1.mostrarEstado();
+        carro2.mostrarEstado();
+
+        // aplicamos freno SOLO al Toyota
+        carro1.frenar();
+
+        System.out.println("--- DESPUÉS DE FRENAR ---");
+        carro1.mostrarEstado();
+        carro2.mostrarEstado();
+    }
+
 }
