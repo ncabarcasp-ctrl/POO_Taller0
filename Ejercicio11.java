a990701ff96248b21e44ee1d6f6d85ff15d4fa76
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 20:57:00 -0500

    feat(bloqueC-ej11): crear clase vehiculo con MostrarEstado

---
+++ Ejercicio11.java
@@ -0,0 +1,12 @@
+class veehiculo {
+    String marca;
+    String modelo;
+    int velocidasActual;
+
+    void mostrarEstado() {
+        System.out.println("Marca: " + marca + " | " + "Modelo: " + modelo + " | " + "Velocidad Actual: " + velocidasActual + " km/h");
+    }
+}
+
+public class Ejercicio11 {
+}
