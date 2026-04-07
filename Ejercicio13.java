6d5f3ea3257d54743798793bb0cdc9165d1a0074
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 22:42:32 -0500

    feat(bloqueC-ej13): crear estructuras base de cuenta bancaria

---
+++ Ejercicio13.java
@@ -0,0 +1,12 @@
+class CuentaBancaria {
+    String numero;
+    String titular;
+    double saldo;
+
+    void mostrarCuenta() {
+        System.out.println("Número: " + numero + " | " + "Titular: " + titular + " | " + "Saldo: $" + saldo);
+    }
+}
+ public class Ejercicio13 {
+ }
+    
