ac3252e1446f970d01afeb34fcb0588b8f28ca87
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 22:51:48 -0500

    feat(bloqueC-ej13): agregar método consignar con validación

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -6,7 +6,14 @@ class CuentaBancaria {
     void mostrarCuenta() {
         System.out.println("Número: " + numero + " | " + "Titular: " + titular + " | " + "Saldo: $" + saldo);
     }
+
+ void consignar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Error: El valor a consignar debe ser mayor que cero.");
+        }
+    }
 }
  public class Ejercicio13 {
- }
-    
+}
