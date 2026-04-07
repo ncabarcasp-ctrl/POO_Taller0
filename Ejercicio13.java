37fd381b3ac2ca688fb3a0ee6d0fc898eeda5bf1
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-06 23:06:54 -0500

    feat(bloqueC-ej13): probar consignación en cuentas independientes

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -16,4 +16,27 @@ class CuentaBancaria {
     }
 }
  public class Ejercicio13 {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria();
+        CuentaBancaria cuenta2 = new CuentaBancaria();
+
+        cuenta1.numero = "123456789";
+        cuenta1.titular = "Natalia Cabarcas";
+        cuenta1.saldo = 1500.75;
+
+        cuenta2.numero = "987654321";
+        cuenta2.titular = "Karol G";
+        cuenta2.saldo = 2500.00;
+        
+        System.out.println("Saldos iniciales:");
+        cuenta1.mostrarCuenta();
+        cuenta2.mostrarCuenta();
+
+        System.out.println("\nConsignando $500 a la cuenta de Natalia Cabarcas...");
+        cuenta1.consignar(500.00);
+
+        System.out.println("Saldos después de la consignación:");
+        cuenta1.mostrarCuenta();
+        cuenta2.mostrarCuenta();
+    }
 }
