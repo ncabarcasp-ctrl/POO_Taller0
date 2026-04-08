55167b3224b96a1c13e01aa766c512f7b327259b
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 16:15:21 -0500

    feat(bloqueG-ej36): instanciar Puerta y probar su cambio de estado

--- src/BloqueG/Ejercicio36.java
+++ src/BloqueG/Ejercicio36.java
@@ -2,21 +2,38 @@ class Puerta {
     String material;
     Boolean abierta;
 
-    void abrir() {
-        abierta = true;
+    public Puerta(String material) {
+        this.material = material;
+        this.abierta = false;
     }
-    void cerrar() {
-        abierta = false;
-    }
-    void mostrarEstado() {
-        if (abierta) {
-            System.out.println("La puerta de " + material + " está ABIERTA");
-        } else {
-            System.out.println("La puerta de " + material + " está CERRADA");
-        }
-    }
+void abrir(){
+    abierta = true;
 }
+
+void cerrar() {
+    abierta = false;
+}
+void mostrarEstado() {
+    if (abierta) {
+        System.out.println("La puera de " + material + " esta ABIERTA");
+    } else {
+        System.out.println("La puerta de" + material + "esta CERRADA");
+    
+    }
+ }
+
+}
+
 public class Ejercicio36 {
     public static void main(String[] args) {
+        Puerta miPuerta = new Puerta("Madera");
+        
+        miPuerta.mostrarEstado(); 
+        
+        miPuerta.abrir();
+        miPuerta.mostrarEstado();
+        
+        miPuerta.cerrar();
+        miPuerta.mostrarEstado();
     }
 }
