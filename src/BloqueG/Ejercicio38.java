c074737b732b68d656695b1124648da434ea2cb0
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 18:21:45 -0500

    feat(bloqueG-ej38): definir clase Ventilador con constructor y metodos de control

---
+++ src/BloqueG/Ejercicio38.java
@@ -0,0 +1,50 @@
+class Ventilador {
+    String marca;
+    int velocidad;
+    boolean encendido;
+    static final int MAX_VELOCIDAD = 3;
+
+    public Ventilador(String marca) {
+        this.marca = marca;
+        this.velocidad = 0;
+        this.encendido = false;
+    }
+
+    public void encender() {
+        if (encendido) {
+            System.out.println("El ventilador ya está encendido.");
+        } else {
+            encendido = true;
+            velocidad = 1;
+        }
+    }
+
+    public void apagar() {
+        encendido = false;
+        velocidad = 0;
+    }
+
+    public void subirVelocidad() {
+        if (!encendido) {
+            System.out.println("No se puede subir velocidad: el ventilador está apagado.");
+            return;
+        }
+
+        if (velocidad < MAX_VELOCIDAD) {
+            velocidad++;
+        } else {
+            System.out.println("Velocidad máxima (" + MAX_VELOCIDAD + ") alcanzada.");
+        }
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Ventilador " + marca +
+                " | Estado: " + (encendido ? "ENCENDIDO" : "APAGADO") +
+                " | Velocidad: " + velocidad);
+    }
+}
+
+public class Ejercicio38 {
+    public static void main(String[] args) {
+    }
+}
