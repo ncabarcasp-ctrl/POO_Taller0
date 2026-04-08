aa09a783bebab315015089d53aa0544f7b50230f
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 00:31:30 -0500

    fix(bloqueF-ej29): corregir valor por defecto de semestre y bug de impresion en main

--- src/BloqueF/Ejercicio29.java
+++ src/BloqueF/Ejercicio29.java
@@ -9,6 +9,7 @@ class Estudiante {
     public Estudiante(String nombre, int codigo) {
         this.nombre = nombre;
         this.codigo = codigo;
+        this.semestre = 1; // Asignar semestre por defecto
     }
 
     public Estudiante(String nombre, int codigo, int semestre) {
@@ -28,5 +29,20 @@ class Estudiante {
 
 public class Ejercicio29 {
     public static void main(String[] args) {
+        Estudiante est1 = new Estudiante();
+        est1.nombre = "Natalia";
+        est1.codigo = 12345;
+        est1.semestre = 2;
+        
+        Estudiante est2 = new Estudiante("Maluma", 54321);
+        Estudiante est3 = new Estudiante("Sabrina Carpenter", 67990, 5);
+        
+        est1.mostrarInfo();
+        est2.mostrarInfo();
+        est3.mostrarInfo();
+
+        est1.cambiarSemestre(3);
+        System.out.println("DESPUES DE CAMBIAR SEMESTRE ");
+        est1.mostrarInfo();
     }
 }
