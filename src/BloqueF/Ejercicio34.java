8a9d4e44a8ecf88efc2f76c2eb3eafa4abbb078b
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 01:41:17 -0500

    feat(bloqueF-ej34): instanciar libro original y generar una copia exacta en el main

--- src/BloqueF/Ejercicio34.java
+++ src/BloqueF/Ejercicio34.java
@@ -19,5 +19,12 @@ class Libro34 {
 
 public class Ejercicio34 {
     public static void main(String[] args) {
+        Libro34 libroOriginal = new Libro34("Orgullo y prejuicio", "Jane Austen");
+        Libro34 libroCopia = new Libro34(libroOriginal);
+
+        System.out.println("--- LIBRO ORIGINAL:");
+        libroOriginal.mostrarLibro();
+        System.out.println("--- LIBRO COPIA:");
+        libroCopia.mostrarLibro();
     }
 }
