1657a92b73e1665b25aef65f9731de9d87141c36
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-07 20:18:40 -0500

    feat(bloqueE-ej23):agregar constructor vacio a ese libro

---
+++ src/bloqueE/Ejercicio23.java
@@ -0,0 +1,15 @@
+class Libro {
+    String titulo;
+    String autor;
+
+    public Libro(String titulo, String autor) {
+        System.out.println("Se ha creado un libro usando el constructor vacío");
+}
+void mostrarLibro() {
+    System.out.println("Libro: " + titulo + " | Autor: " + autor);
+}
+
+}
+public class Ejercicio23 {
+    
+}
