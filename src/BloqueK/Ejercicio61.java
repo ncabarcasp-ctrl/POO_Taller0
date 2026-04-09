4e1d87d15694949803c7f8b74309f6a294ecef5a
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-09 15:53:54 -0500

    (bloqueK-ej61): crear documento de diseno para la clase Docente detallando atributos, metodos e instancias

---
+++ src/BloqueK/Ejercicio61.java
@@ -0,0 +1,27 @@
+// Diseño de la Clase: Docente
+
+//1. Atributos (Lo que el docente ES o TIENE)
+// nombre (`String`): El nombre completo del profesor.
+// asignatura (`String`): La materia principal que imparte.
+// añosExperiencia (`int`): Cuántos años lleva dando clases.
+// activo (`boolean`): Si actualmente está dando clases en la institución o está de vacaciones/licencia.
+
+//2. Métodos (Lo que el docente HACE)
+// darClase(): Imprime un mensaje indicando que el docente está impartiendo su asignatura.
+// calificarExamen(double nota): Recibe una nota y determina si un alumno aprobó o reprobó.
+// mostrarPerfil(): Muestra un resumen con todos los datos del docente.
+
+// 3. Objetos Reales (Instancias que nacerán de este molde)
+// Objeto 1:  Nombre: "Alan Turing"
+  //Asignatura: "Criptografía"
+  // Años de Experiencia: 10
+  // Activo: true
+
+// bjeto 2: Nombre: "Marie Curie"
+  // Asignatura: "Física y Química"
+// Años de Experiencia: 15
+  // Activo: true
+
+  // CODIGO:
+
+  
