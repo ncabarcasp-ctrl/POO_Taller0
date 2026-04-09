b237953d8b30ca8c38c3c40c472eddbff4758802
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:45:09 -0500

    fix(bloqueI-ej47): instanciar el objeto Reloj usando new antes de llamar a sus metodos

--- src/BloqueI/Ejercicio47.java
+++ src/BloqueI/Ejercicio47.java
@@ -1,3 +1,22 @@
+// CODIGO INCORRECTO
+//class Reloj {
+   // String marca;
+    //public Reloj(String marca) {
+        //this.marca = marca;
+    //}
+    //void darHora() {
+        //System.out.println("El reloj " + marca + " marca las 12:00");
+    //}
+//}
+
+//public class Ejercicio47 {
+    //public static void main(String[] args) {
+        //Reloj miReloj;
+        
+        //miReloj.darHora();
+    //}
+//}
+
 class Reloj {
     String marca;
 
@@ -12,7 +31,7 @@ class Reloj {
 
 public class Ejercicio47 {
     public static void main(String[] args) {
-        Reloj miReloj;
+        Reloj miReloj = new Reloj("Casio");
         
         miReloj.darHora();
     }
