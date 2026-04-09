f4810bb355a4e682c150c990610997f03bf75f9b
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:15:59 -0500

    fix(bloqueI-ej46): agregar el nombre identificador faltante a la declaracion de la clase

--- src/BloqueI/Ejercio46.java
+++ src/BloqueI/Ejercio46.java
@@ -1,4 +1,13 @@
-class {
+// VERSION INCORRECTA
+//class {
+    //String nombre;
+
+    //public Gato(String nombre) {
+    //    this.nombre = nombre;
+    //}
+//}
+
+class Gato {
     String nombre;
 
     public Gato(String nombre) {
