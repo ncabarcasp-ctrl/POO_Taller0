c0a92132f9c7884867a165cdf3e61503dc5a72e4
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-09 00:40:14 -0500

    feat(bloqueJ-ej54): definir clase Semaforo con logica condicional para transicion de colores

---
+++ src/BloqueJ/Ejercicio54.java
@@ -0,0 +1,30 @@
+// Ejercicio 53: Crea una clase Reserva con: nombreCliente,fecha, activa. Agrega métodos para confirmar y cancelar
+
+class Semaforo {
+    String color;
+
+    public Semaforo(String color) {
+        this.color = color;
+    }
+
+    void cambiarColor() {
+        if (color.equals("Rojo")) {
+            color = "Verde";
+        } else if (color.equals("Verde")) {
+            color = "Amarillo";
+        } else if (color.equals("Amarillo")) {
+            color = "Rojo";
+        } else {
+            color = "Rojo";
+        }
+    }
+
+    void mostrarEstado() {
+        System.out.println("Estado del semáforo: " + color);
+    }
+}
+
+public class Ejercicio54 {
+    public static void main(String[] args) {
+    }
+}
