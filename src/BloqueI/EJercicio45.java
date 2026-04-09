269e22f017488d375055f15b9861bc39390e8348
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 21:47:29 -0500

    fix(bloqueI-ej45): mover método maullar dentro de la clase Gato

--- src/BloqueI/EJercicio45.java
+++ src/BloqueI/EJercicio45.java
@@ -4,9 +4,8 @@ class Gato {
     public Gato(String nombre) {
         this.nombre = nombre;
     }
-}
 
-// MAL: método fuera de la clase
-void maullar() {
-    System.out.println("Miau");
+    void maullar() {
+        System.out.println(nombre + " dice: ¡Miau!");
+    }
 }
