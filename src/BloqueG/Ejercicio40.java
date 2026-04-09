75ebed9bebdc4e2a9a563e5c687f62ad109ab834
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 19:31:30 -0500

    feat(bloqueG-ej40): instanciar objetos Pelicula y probar el metodo mostrarFicha

--- src/BloqueG/Ejercicio40.java
+++ src/BloqueG/Ejercicio40.java
@@ -18,6 +18,10 @@ class Pelicula {
 
 public class Ejercicio40 {
     public static void main(String[] args) {
-        
+        Pelicula peli1 = new Pelicula("Las Tortugas Ninja 2", "Ciencia Ficcion", 112);
+        Pelicula peli2 = new Pelicula("Shrek", "Animación", 90);
+
+        peli1.mostrarFicha();
+        peli2.mostrarFicha();
     }
 }
