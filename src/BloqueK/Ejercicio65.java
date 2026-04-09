2312f95aa3806699abf17d4bbb37ceca815e8e32
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-09 17:34:36 -0500

    (bloqueK-ej65): crear disñno para la clase EquipoDeFutbol

---
+++ src/BloqueK/Ejercicio65.java
@@ -0,0 +1,23 @@
+// Diseño de la Clase: EquipoDeFutbol
+
+//1. Atributos (Estado)
+// nombre (`String`): El nombre oficial del club.
+// estadio (`String`): La sede donde juegan de local.
+// puntos (`int`): La cantidad de unidades acumuladas en el torneo.
+// enPrimeraDivision (`boolean`): Indica si el equipo juega en la categoría A (`true`) o en la B (`false`).
+
+// 2. Métodos (Comportamiento)
+// ganarPartido(): Suma 3 al atributo `puntos`.
+// empatarPartido(): Suma 1 al atributo `puntos`.
+/// mostrarEstadisticas(): Imprime la información del club y su puntaje actual.
+
+// 3. Objetos Reales (Instancias)
+// Objeto 1: Nombre: "Real Cartagena"
+  // Estadio: "Jaime Morón León"
+  // Puntos: 0
+  // En Primera División: false
+
+// Objeto 2: Nombre: "Junior de Barranquilla"
+  // Estadio: "Metropolitano Roberto Meléndez"
+  // Puntos: 15
+  // En Primera División: true
