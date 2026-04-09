da01de11c50350ab8141c5c98b13beecfe92ccf6
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:53:50 -0500

    feat(bloqueI-ej48): implementar clase Billetera (con error de variable local en el metodo)

---
+++ src/BloqueI/Ejercicio48.java
@@ -0,0 +1,25 @@
+class Billetera {
+    int dinero;
+
+    public Billetera(int dineroInicial) {
+        this.dinero = dineroInicial;
+    }
+
+    void agregarDinero(int cantidad) {
+        int dinero = this.dinero + cantidad;
+    }
+
+    void mostrarSaldo() {
+        System.out.println("Saldo actual: $" + dinero);
+    }
+}
+
+public class Ejercicio48 {
+    public static void main(String[] args) {
+        Billetera miBilletera = new Billetera(100);
+        
+        miBilletera.agregarDinero(50);
+        
+        miBilletera.mostrarSaldo();
+    }
+}
