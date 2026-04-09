ef0434a998c06027f4b2d25275fd3ad830b1ddb3
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-09 00:23:33 -0500

    feat(bloqueJ-ej53): definir clase Reserva con constructor y metodos para alterar su estado

---
+++ src/BloqueJ/Ejercicio53.java
@@ -0,0 +1,36 @@
+//Ejercicio 53: Crea una clase Reserva con: nombreCliente, fecha, activa, Agrega métodos para confirmar y cancelar
+
+class Reserva {
+    String nombreCliente;
+    String fecha;
+    boolean activa;
+
+    public Reserva(String nombreCliente, String fecha, boolean activa) {
+        this.nombreCliente = nombreCliente;
+        this.fecha = fecha;
+        this.activa = activa;
+    }
+
+    void confirmar() {
+        activa = true;
+        System.out.println(" Acción: Confirmando reserva...");
+    }
+
+    void cancelar() {
+        activa = false;
+        System.out.println(" Acción: Cancelando reserva...");
+    }
+
+    void mostrarEstado() {
+        if (activa) {
+            System.out.println("Reserva de " + nombreCliente + " para el " + fecha + " | Estado: CONFIRMADA");
+        } else {
+            System.out.println("Reserva de " + nombreCliente + " para el " + fecha + " | Estado: CANCELADA");
+        }
+    }
+}
+
+public class Ejercicio53 {
+    public static void main(String[] args) {
+    }
+}
