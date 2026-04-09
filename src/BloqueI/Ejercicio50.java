ca0251bf907c7660b9c0dba2a74d920fa93c8461
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 23:28:39 -0500

    fix(bloqueI-ej50): instanciar el objeto Libro en main para ejecutar su comportamiento

--- src/BloqueI/Ejercicio50.java
+++ src/BloqueI/Ejercicio50.java
@@ -1,5 +1,25 @@
 // El Código Incompleto
 
+// class Libro {
+   // String titulo;
+
+    // public Libro(String titulo) {
+       // this.titulo = titulo;
+    // }
+
+    // void abrir() {
+       //  System.out.println("Abriendo el libro: " + titulo);
+    //}
+//}
+
+//public class Ejercicio50 {
+  //  public static void main(String[] args) {
+    //    System.out.println("¡Bienvenido a la biblioteca!");
+    //}
+//}
+
+// El Código Corregido
+
 class Libro {
     String titulo;
 
@@ -15,5 +35,9 @@ class Libro {
 public class Ejercicio50 {
     public static void main(String[] args) {
         System.out.println("¡Bienvenido a la biblioteca!");
+        
+        Libro miLibro = new Libro("El Principito");
+        
+        miLibro.abrir();
     }
 }
