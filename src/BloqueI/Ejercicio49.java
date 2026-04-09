2b641142499f4d1f78af9865e649b21f7faf4b37
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 23:20:32 -0500

    fix(bloqueI-ej49): agregar parametro de referencia al metodo atacar para interactuar entre objetos

--- src/BloqueI/Ejercicio49.java
+++ src/BloqueI/Ejercicio49.java
@@ -1,5 +1,29 @@
 // CODIGO ERRONEO /Corrige un ejemplo donde un objeto intenta acceder a atributos de otro sin referencia adecuada.
 
+//class Jugador {
+   // String nombre;
+    //int vida;
+
+    //public Jugador(String nombre, int vida) {
+      //  this.nombre = nombre;
+        //this.vida = vida;
+    //}
+
+    //void atacar() {
+      //  enemigo.vida = enemigo.vida - 10;
+        //System.out.println(nombre + " atacó al enemigo. Le quedan " + enemigo.vida + " puntos de vida.");
+    //}
+//}
+
+//public class Ejercicio49 {
+  //  public static void main(String[] args) {
+    //    Jugador heroe = new Jugador("Link", 100);
+      //  Jugador enemigo = new Jugador("Ganon", 100);
+        
+        //heroe.atacar();
+    //}
+//}
+
 class Jugador {
     String nombre;
     int vida;
@@ -9,9 +33,9 @@ class Jugador {
         this.vida = vida;
     }
 
-    void atacar() {
-        enemigo.vida = enemigo.vida - 10;
-        System.out.println(nombre + " atacó al enemigo. Le quedan " + enemigo.vida + " puntos de vida.");
+    void atacar(Jugador objetivo) {
+        objetivo.vida = objetivo.vida - 10;
+        System.out.println(nombre + " atacó a " + objetivo.nombre + ". Vida restante: " + objetivo.vida);
     }
 }
 
@@ -20,6 +44,6 @@ public class Ejercicio49 {
         Jugador heroe = new Jugador("Link", 100);
         Jugador enemigo = new Jugador("Ganon", 100);
         
-        heroe.atacar();
+        heroe.atacar(enemigo);
     }
 }
