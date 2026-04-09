fd76d004ab0690776047a1943f77774de3ed2452
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:22:48 -0500

    feat(bloqueI-ej46): instanciar objeto Gato  para comprobar que la clase compila correctamente.

--- src/BloqueI/Ejercicio46.java
+++ src/BloqueI/Ejercicio46.java
@@ -6,11 +6,24 @@
     //    this.nombre = nombre;
     //}
 //}
+// VERSION CORREGIDA
 
 class Gato {
     String nombre;
 
     public Gato(String nombre) {
         this.nombre = nombre;
+    }
+    
+    void maullar() {
+        System.out.println(nombre + " dice: ¡Miau!");
+    }
+}
+
+
+public class Ejercicio46 {
+    public static void main(String[] args) {
+        Gato g1 = new Gato("Michi");
+        g1.maullar();
     }
 }
