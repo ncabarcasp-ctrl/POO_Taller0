b9a6acc7deca70426b0b07526482cdec69ac87f5
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 23:27:00 -0500

    feat(bloqueI-ej50): implementar clase Libro (sin instanciar el objeto en main)

---
+++ src/BloqueI/Ejercicio50.java
@@ -0,0 +1,19 @@
+// El Código Incompleto
+
+class Libro {
+    String titulo;
+
+    public Libro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    void abrir() {
+        System.out.println("Abriendo el libro: " + titulo);
+    }
+}
+
+public class Ejercicio50 {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido a la biblioteca!");
+    }
+}
