9d4d3408474471bd5024d49c72ff1fab00abcd62
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:56:25 -0500

    fix(bloqueI-ej48): eliminar declaracion de variable local para actualizar correctamente el atributo de clase

--- src/BloqueI/Ejercicio48.java
+++ src/BloqueI/Ejercicio48.java
@@ -1,3 +1,28 @@
+//CODIGO ERROR 
+// class Billetera {
+    // int dinero;
+    // public Billetera(int dineroInicial) {
+       // this.dinero = dineroInicial;
+    //}
+    //void agregarDinero(int cantidad) {
+      //  int dinero = this.dinero + cantidad;
+    //}
+
+    //void mostrarSaldo() {
+      //  System.out.println("Saldo actual: $" + dinero);
+    //}
+//}
+
+//public class Ejercicio48 {
+  //  public static void main(String[] args) {
+    //    Billetera miBilletera = new Billetera(100);
+        
+      //  miBilletera.agregarDinero(50);
+        
+        // miBilletera.mostrarSaldo();
+    // }
+// }
+// CODIGO CORREGIDO:
 class Billetera {
     int dinero;
 
@@ -6,7 +31,7 @@ class Billetera {
     }
 
     void agregarDinero(int cantidad) {
-        int dinero = this.dinero + cantidad;
+        this.dinero = this.dinero + cantidad;
     }
 
     void mostrarSaldo() {
