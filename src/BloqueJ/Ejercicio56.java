847045f72cd3890803e1077a135f84e51d7cb34a
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-09 14:37:37 -0500

    feat(bloqueJ-ej56): definir clase Temperatura con logica matematica para convertir a Fahrenheit

---
+++ src/BloqueJ/Ejercicio56.java
@@ -0,0 +1,20 @@
+class Temperatura {
+    double celsius;
+
+    public Temperatura(double celsius) {
+        this.celsius = celsius;
+    }
+
+    double convertirAFahrenheit() {
+        return (celsius * 1.8) + 32;
+    }
+
+    void mostrarConversion() {
+        System.out.println("Temperatura: " + celsius + "°C equivalen a " + convertirAFahrenheit() + "°F");
+    }
+}
+
+public class Ejercicio56 {
+    public static void main(String[] args) {
+    }
+}
