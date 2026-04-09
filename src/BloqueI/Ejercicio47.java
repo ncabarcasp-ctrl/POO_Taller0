308f0e7c0c655facfcdb3a527496fce53a75672c
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 22:42:15 -0500

    feat(bloqueI-ej47): implementar clase Reloj y probar uso de objeto (con error de inicializacion)

---
+++ src/BloqueI/Ejercicio47.java
@@ -0,0 +1,19 @@
+class Reloj {
+    String marca;
+
+    public Reloj(String marca) {
+        this.marca = marca;
+    }
+
+    void darHora() {
+        System.out.println("El reloj " + marca + " marca las 12:00");
+    }
+}
+
+public class Ejercicio47 {
+    public static void main(String[] args) {
+        Reloj miReloj;
+        
+        miReloj.darHora();
+    }
+}
