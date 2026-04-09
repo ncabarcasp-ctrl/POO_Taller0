fc7d660a4e50c6e18178b6967557a17e57de2976
NAT <ncabarcasp@unicartagena.edu.co>
2026-04-08 23:49:18 -0500

    feat(bloqueJ-ej51): definir clase Rectangulo con constructor y metodos de calculo matematico

---
+++ src/BloqueJ/Ejercicio51.java
@@ -0,0 +1,30 @@
+class Rectangulo {
+    double base;
+    double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    double calcularArea() {
+        return base * altura;
+    }
+
+    double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+
+    void mostrarResultados() {
+        System.out.println("--- DATOS DEL RECTÁNGULO ---");
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+    }
+}
+
+public class Ejercicio51 {
+    public static void main(String[] args) {
+    }
+}
